1798e12c6be787626c0be91d0a4c958a7cda04dc
alvadeli <142695804+alvadeli@users.noreply.github.com>
2023-09-12 12:59:18 +0200

    Finished Day03

---
+++ 2022/cs_AOC/day03/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace day03
+{
+    internal class Program
+    {
+        public static bool demo = false;
+        public static bool firstPart = false;
+
+        static void Main(string[] args)
+        {
+            string appDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string filename = demo ? "demo-input.txt" : "input.txt";
+            string file = appDirectory + filename;
+
+            string[] rucksacks = File.ReadAllLines(file);
+
+            int rucksacksPrioritySum;
+            if (firstPart)
+            {
+                rucksacksPrioritySum = RucksackPriority.GetPrioritiesSum(rucksacks);
+            } 
+            else
+            {
+                rucksacksPrioritySum = RucksackPriority.GetGroupPrioritiesSum(rucksacks);
+            }
+
+            Console.WriteLine("Rucksacks Priority Sum: {0}", rucksacksPrioritySum);
+
+
+        }
+
+
+    }  
+}
---
+++ 2022/cs_AOC/day03/RucksackPriority.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace day03
+{
+    internal static class RucksackPriority
+    {
+        private const char CharWhiteSpace = ' ';
+
+        public static int GetPrioritiesSum(string[] rucksacks)
+        {
+            int sum = 0;
+            foreach (string rucksack in rucksacks)
+            {
+                sum += GetSingleRucksackPriority(rucksack);
+            }
+            return sum;
+        }
+
+
+        public static int GetGroupPrioritiesSum(string[] rucksacks) 
+        {
+            const int groupSize = 3;
+            int sum= 0;
+
+            for (int i = 0; i < rucksacks.Count(); i+=groupSize) 
+            {
+                string[] group = new string[groupSize] { rucksacks[i], rucksacks[i+1], rucksacks[i+2] };
+                sum += GetGroupRucksackPriority(group);
+            }
+            return sum;
+
+        }
+
+        private static int GetSingleRucksackPriority(string rucksack)
+        {
+            char commonItem = GetCommonRucksackItem(rucksack);
+            return GetPriorityValue(commonItem);
+        }
+
+        private static int GetGroupRucksackPriority(string[] rucksackGroup) 
+        {
+            char commonItem = GetCommonGroupItem(rucksackGroup);
+            return GetPriorityValue(commonItem);
+        }
+
+        private static char GetCommonRucksackItem(string rucksack)
+        {
+            int middleIndex = rucksack.Length / 2;
+
+            string firstCompartment = rucksack[..middleIndex];
+            string secondCompartment = rucksack[middleIndex..];
+
+            foreach (var item in firstCompartment)
+            {
+                if (secondCompartment.Contains(item))
+                {
+                    return item;
+                }
+            }
+
+            return CharWhiteSpace;
+        }
+
+        private static char GetCommonGroupItem(string[] rucksackGroup)
+        {
+            foreach (var item in rucksackGroup[0])
+            {
+                if (rucksackGroup[1].Contains(item) && rucksackGroup[2].Contains(item))
+                {
+                    return item;
+                }
+            }
+            return CharWhiteSpace;
+        }
+
+        static int GetPriorityValue(char item)
+        {
+            if (char.IsWhiteSpace(item))
+            {
+                return 0;
+            }
+
+            if (char.IsLower(item))
+            {
+                // Lowercase item types a through z have priorities 1 through 26.
+                // Ascii Offset: e.g for a -> 97 - 96 = 1
+                int lowercaseAsiiOffset = -96;
+                return item + lowercaseAsiiOffset;
+            }
+
+            // Uppercase item types A through Z have priorities 27 through 52.
+            // Ascii Offset: e.g for A -> 65 - 38 = 27
+            int UpperCaseAsciiOffset = -38;
+            return item + UpperCaseAsciiOffset;
+        }
+    }
+}
