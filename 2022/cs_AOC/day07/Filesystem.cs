a132913bdcee8442fa4a49594fc2599b6884977e
alvadeli <142695804+alvadeli@users.noreply.github.com>
2023-09-18 20:28:24 +0200

    Removed unnessary usings

--- 2022/cs_AOC/day07/Filesystem.cs
+++ 2022/cs_AOC/day07/Filesystem.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Data;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Reflection.Metadata;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data;
 
 namespace day07
 {
